3264bf33ec4aa441a354985780b17f6d67c587f5
Jean-Olivier Irisson <irisson@normalesup.org>
2015-02-18 11:33:32 +0100

    Further image io tests
    => use OpenCV!

--- tests/test-io_image.py
+++ tests/test-io_image.py
@@ -4,12 +4,25 @@ import numpy as np
 import cv2 as cv2
 import skimage.io as io
 
-img = np.random.randint(0,255,(20480,2048))
+# io.find_available_plugins()
+# # io.available_plugins
+# {'null': ['imshow', 'imread', 'imsave', 'imread_collection'], 'pil': ['imread', 'imsave', 'imshow', 'imread_collection'], 'qt': ['imshow', 'imsave', 'imread', 'imread_collection'], 'freeimage': ['imread', 'imsave', 'imread_collection'], 'gtk': ['imshow'], 'matplotlib': ['imshow', 'imread', 'imread_collection'], 'test': ['imsave', 'imshow', 'imread', 'imread_collection'], 'simpleitk': ['imread', 'imsave', 'imread_collection'], 'imread': ['imread', 'imsave', 'imread_collection'], 'fits': ['imread', 'imread_collection'], 'tifffile': ['imread', 'imsave', 'imread_collection'], 'gdal': ['imread', 'imread_collection']}
+# io.use_plugin('imread')
 
+
+## Test writing image -----------------------------------------------------
+
+# generate a fake image
+img = np.random.randint(0,255,(2048*3,2048))
+
+# save it 20 times
+# io.use_plugin('pil')
+# io.use_plugin('matplotlib')
 for i in range(0,20):
     # cv2.imwrite(str(i) + '.tiff', img)
-    cv2.imwrite(str(i) + '.png', img)
+    # cv2.imwrite(str(i) + '.png', img)
     # cv2.imwrite(str(i) + '.jpg', img)
+    # io.imsave(str(i) + '.png', img)
 
 # cv2, 2048x2048
 # tiff: 8.5s
@@ -23,12 +36,14 @@ for i in range(0,20):
 
 # -> png wins
 
+# cv2 png vs skimage png, 2048*3,2048
+# cv2 4.044s
+# pil 24.648s
+# matplotlib 24.702s
 
-# TODO test skimage.io.imwrite
-# io.find_available_plugins()
-# # io.available_plugins
-# io.use_plugin('imread')
-# for i in range(0,100):
-#     io.imsave(str(i) + '.tiff', img, )
+# -> cv2 wins by a large margin
+
+## Test reading image -----------------------------------------------------
 
 # TODO test skimage.io.imread vs cv2.imread
+
