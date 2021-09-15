baf6bd18e52c2bc496cb5a0e8ccedcf9b2075151
Thelma Panaiotis <thelma@crans.org>
2021-09-15 09:36:48 +0000

    Faster prediction overlap resolution

--- apeep/semantic.py
+++ apeep/semantic.py
@@ -58,12 +58,6 @@ def semantic_segment(img, gray_threshold, predictor, sem_upsample_size, sem_n_ba
         predictor=predictor
     )
     
-    # resolve overlaps
-    predictions = resolve_overlaps(
-        img_preds=predictions,
-        frames_props=frames_props,
-    )
-    
     # generate new image with ROIs only
     img_rois = extract_rois(
         img=img,
@@ -298,295 +292,6 @@ def predict_frames(frames, frames_props, n_batches, predictor):
     return(preds)
 
 
-def resolve_overlaps(img_preds, frames_props, overlap_threshold=0.2):
-    """
-    Resolve semantic prediction overlaps with following steps:
-    1. Detect bbox overlaps -> potential overlaps
-    2. Check mask overlaps -> true and false overlaps
-    3. Process false overlaps
-    3. Resolve true overlaps
-
-    Args:
-        img_preds (df): semantic predictions with frame and pred info
-        frames_props (dict): dict of frames label and coordinates
-        overlap_threshold (float): mask iou between to predictions to consider an overlap. In range ]0,1]
-
-    Returns:
-        DataFrame: semantic predictions without overlapping predictions
-    """
-    # Convert frames properties to dataframe
-    frames_props = pd.DataFrame(frames_props)
-    
-    ## Detect bbox overlaps to list potential overlaps
-    # Initiate empty dict to store potential overlapping predictions 
-    pot_overlaps = {
-        'i': [],
-        'j': [],
-    }
-    # Loop over predictions and test bbox overlap for each prediction pair
-    for i in img_preds.index:
-        for j in img_preds.index:
-            if j > i:
-                # Check bbox interception
-                inter = check_bbox_overlap(
-                    img_preds['bbox_corr'][i].tolist(), 
-                    img_preds['bbox_corr'][j].tolist()
-                )
-                # In case of bbox interception, store prediction index
-                if inter:
-                    pot_overlaps['i'].append(i)
-                    pot_overlaps['j'].append(j)
-
-    # Convert to dataframe
-    pot_overlaps = pd.DataFrame(pot_overlaps)
-    # Add a column for overlap status
-    pot_overlaps['same'] = False
-    # And one to store bbox union of potential overlaps
-    pot_overlaps['bbox_union'] = np.empty((len(pot_overlaps), 0)).tolist()
-
-    # Create a new df with only non overlapping predictions
-    preds_ok = img_preds[~img_preds.index.isin(np.unique(pot_overlaps[['i', 'j']]))].reset_index(drop = 'True')
-    # Drop bbox and mask as we won't need these
-    preds_ok = preds_ok.drop(['bbox', 'mask'], axis=1)
-    
-    ## Check mask overlap to distinct between true and false overlaps
-    # Loop over potential overlaps
-    for t in pot_overlaps.index:
-        # Extract overlaping prediction indexes
-        i = pot_overlaps.loc[t,'i']
-        j = pot_overlaps.loc[t,'j']
-        
-        # Determine overlapping case between:
-        # - case 1: frame 1 is the same as frame 2
-        # - case 2: frame 1 is at left of frame 2
-        # - case 3: frame 1 is on top of frame 2
-        # - case 4: frame 1 is at top-left diagonal of frame 2
-        # - case 5: frame 1 is at top-right diagonal of frame 2
-        
-        # Extract frames row and column indexes
-        frame_row_i = img_preds['frame_row'][i]
-        frame_col_i = img_preds['frame_col'][i]
-        frame_row_j = img_preds['frame_row'][j]
-        frame_col_j = img_preds['frame_col'][j]
-        
-        # Case 1:
-        if (frame_col_i == frame_col_j) & (frame_row_i == frame_row_j):
-            # Frame 1 is the same as frame 2
-            # Extract masks of overlap area
-            mask_over_i = img_preds['mask'][i]
-            mask_over_j = img_preds['mask'][j]            
-        
-        # Case 2: 
-        if (frame_col_i < frame_col_j) & (frame_row_i == frame_row_j):
-            # Frame 1 is at left of frame 2
-            # Compute horizontal overlap
-            col1_i = frames_props.loc[(frames_props['frame_row'] == frame_row_i) & (frames_props['frame_col'] == frame_col_i), 'col1'].values[0] # right column of frame 1
-            col0_j = frames_props.loc[(frames_props['frame_row'] == frame_row_j) & (frames_props['frame_col'] == frame_col_j), 'col0'].values[0] # left column of frame 2
-            over_h = int(col1_i - col0_j) # horizontal overlap
-            
-            # Extract masks of overlap area
-            mask_over_i = img_preds['mask'][i][:, -over_h-1:-1]
-            mask_over_j = img_preds['mask'][j][:, 0:over_h]
-        
-        # Case 3: 
-        if (frame_col_i == frame_col_j) & (frame_row_i < frame_row_j):
-            # Frame 1 is on top of frame 2
-            # Compute vertical overlap
-            row1_i = frames_props.loc[(frames_props['frame_row'] == frame_row_i) & (frames_props['frame_col'] == frame_col_i), 'row1'].values[0] # bottom row of frame 1
-            row0_j = frames_props.loc[(frames_props['frame_row'] == frame_row_j) & (frames_props['frame_col'] == frame_col_j), 'row0'].values[0] # top row of frame 1
-            over_v = int(row1_i - row0_j) # vertical overlap
-            
-            # Extract masks of overlap area
-            mask_over_i = img_preds['mask'][i][-over_v-1:-1, :]
-            mask_over_j = img_preds['mask'][j][0:over_v,:]
-            
-        # Case 4:
-        if (frame_col_i < frame_col_j) & (frame_row_i < frame_row_j):
-            # Frame 1 is on top-left diagonal of frame 2
-            # Compute horizontal and vertical overlap
-            col1_i = frames_props.loc[(frames_props['frame_row'] == frame_row_i) & (frames_props['frame_col'] == frame_col_i), 'col1'].values[0] # right column of frame 1
-            col0_j = frames_props.loc[(frames_props['frame_row'] == frame_row_j) & (frames_props['frame_col'] == frame_col_j), 'col0'].values[0] # left column of frame 2
-            over_h = int(col1_i - col0_j) # horizontal overlap
-            row1_i = frames_props.loc[(frames_props['frame_row'] == frame_row_i) & (frames_props['frame_col'] == frame_col_i), 'row1'].values[0] # bottom row of frame 1
-            row0_j = frames_props.loc[(frames_props['frame_row'] == frame_row_j) & (frames_props['frame_col'] == frame_col_j), 'row0'].values[0] # top row of frame 1
-            over_v = int(row1_i - row0_j) # vertical overlap
-            
-            # Extract masks of overlap area
-            mask_over_i = img_preds['mask'][i][-over_v-1:-1, -over_h-1:-1]
-            mask_over_j = img_preds['mask'][j][0:over_v,0:over_h]
-            
-        # Case 5:
-        if (frame_col_i > frame_col_j) & (frame_row_i < frame_row_j):
-            # Frame 1 is on top-right diagonal of frame 2
-            # Compute horizontal and vertical overlap
-            col0_i = frames_props.loc[(frames_props['frame_row'] == frame_row_i) & (frames_props['frame_col'] == frame_col_i), 'col0'].values[0] # left column of frame 1
-            col1_j = frames_props.loc[(frames_props['frame_row'] == frame_row_j) & (frames_props['frame_col'] == frame_col_j), 'col1'].values[0] # right column of frame 2
-            over_h = int(col1_j - col0_i) # horizontal overlap
-            row1_i = frames_props.loc[(frames_props['frame_row'] == frame_row_i) & (frames_props['frame_col'] == frame_col_i), 'row1'].values[0] # bottom row of frame 1
-            row0_j = frames_props.loc[(frames_props['frame_row'] == frame_row_j) & (frames_props['frame_col'] == frame_col_j), 'row0'].values[0] # top row of frame 1
-            over_v = int(row1_i - row0_j) # vertical overlap
-            
-            # Extract masks of overlap area
-            mask_over_i = img_preds['mask'][i][-over_v-1:-1, 0:over_h]
-            mask_over_j = img_preds['mask'][j][0:over_v,-over_h-1:-1]
-            
-        # Compute mask intersection over union (iou)
-        mask_inter = np.sum(np.logical_and(mask_over_i, mask_over_j))
-        mask_union = np.sum(np.logical_or(mask_over_i, mask_over_j))
-        iou = mask_inter / mask_union
-        
-        # If iou is higher than overlap threshold, consider it as an overlap
-        if iou > overlap_threshold: # it's an overlap
-            # Save it as a true overlap
-            pot_overlaps.loc[t, 'same'] = True
-            
-            # Compute bbox union
-            bbox_corr_i = img_preds['bbox_corr'][i].tolist()
-            bbox_corr_j = img_preds['bbox_corr'][j].tolist()
-            bbox_corr = compute_bbox_union(bbox_corr_i, bbox_corr_j)
-            
-            # And write bbox to df
-            pot_overlaps.at[t, 'bbox_union'].extend(bbox_corr)
-
-    ## Process false overlaps: store them with non overlapping predictions
-    # Extract cases which are not overlaps
-    not_overlaps = pot_overlaps[~pot_overlaps['same']]
-    # List predictions which are not overlaps
-    preds_not_overlaps = np.unique(not_overlaps[['i', 'j']])
-    
-    # Make sure these predictions do not overlap with other particles at some point
-    preds = [] # initiate empty list to store preds which are not overlaps
-    for pred in preds_not_overlaps: # loop over predictions which are supposed to not be overlaps
-        over = pot_overlaps[(pot_overlaps['i'] == pred) | (pot_overlaps['j'] == pred)]['same'].any() # check if pred was associated with another overlap
-        if not over: # if it was not, store it with non overlapping preds
-            preds.append(pred) 
-        
-    # Loop over predictions and create entries for these
-    for pred in preds:
-        entry = {
-            'frame_label': img_preds['frame_label'][pred],
-            'frame_row': img_preds['frame_row'][pred],
-            'frame_col': img_preds['frame_col'][pred],
-            'score': img_preds['score'][pred],
-            'bbox_corr': img_preds['bbox_corr'][pred],
-            'pred_index': pred,
-        }        
-        # Store prediction with non overlapping predictions
-        preds_ok = preds_ok.append(entry, ignore_index=True)
-    
-    ## Process cases of 1 big prediction encompassing 2 smaller ones which do not overlap: in this case we want to keep the two small ones
-    # If masks of the two small predictions do not overlap, we can and we want to separate them
-    all_large_preds = [] # initiate list to store large encompassing predictions
-    for n in not_overlaps.index:
-        # extract prediction indexes
-        i = not_overlaps.loc[n, 'i']
-        j = not_overlaps.loc[n, 'j']
-        
-        # We want overlaps that matches with both i and j  while i and j are NOT overlaps
-        # predictions overlapping with i
-        over_i = np.setdiff1d(np.unique(pot_overlaps[((pot_overlaps['i'] == i) | (pot_overlaps['j'] == i)) & (pot_overlaps['same'] == True)][['i', 'j']]), i)
-        # predictions overlapping with j
-        over_j = np.setdiff1d(np.unique(pot_overlaps[((pot_overlaps['i'] == j) | (pot_overlaps['j'] == j)) & (pot_overlaps['same'] == True)][['i', 'j']]), j)
-        # predictions overlapping with both i and j
-        large_preds = np.intersect1d(over_i, over_j).tolist() 
-        all_large_preds.extend(large_preds) # add these predictions to the list of encompassing predictions
-        
-    all_large_preds = list(set(all_large_preds)) # make list unique
-    
-    # and finally ignore these large predictions by removing them from potentiel overlaps
-    pot_overlaps = pot_overlaps[~((pot_overlaps['i'].isin(all_large_preds)) | (pot_overlaps['j'].isin(all_large_preds)))].reset_index(drop=True)
-    
-    ## Resolve cases wich are true overlaps
-    # Extract cases which are true overlaps
-    overlaps = pot_overlaps[pot_overlaps['same']]
-    # Create a column for solving status and initiate it to False
-    overlaps = overlaps.assign(solved = False)
-    
-    # Loop over true overlaps and process them
-    for t in overlaps.index:
-        
-        # Check of overlap is already solved
-        if not overlaps.loc[t, 'solved']:
-            # If not solve, process it
-            
-            # First, determine if it's a simple or multiple overlap
-            # Check how many times pred i and j appear in the dataframe
-            i = overlaps.loc[t, 'i']
-            j = overlaps.loc[t, 'j']
-            
-            # Number of times pred i was considered as an overlap
-            nb_i = sum(overlaps['i'] == i) + sum(overlaps['j'] == i)
-        
-            # Number of times pred j was considered as an overlap
-            nb_j = sum(overlaps['i'] == j) + sum(overlaps['j'] == j)
-            
-            # Case of simple overlap: both predictions are only once in overlaps
-            if (nb_i == 1) and (nb_j == 1):
-                # Prepare new entry for df of non overlapping predictions
-                # Keep info of first frame for label, frame row and column
-                # Compute average of score
-                # Use newly computed bbox_corr
-                entry = {
-                    'frame_label': img_preds['frame_label'][i],
-                    'frame_row': img_preds['frame_row'][i],
-                    'frame_col': img_preds['frame_col'][i],
-                    'score': np.mean([(img_preds['score'][i], img_preds['score'][j])]),
-                    'bbox_corr': overlaps['bbox_union'][t],
-                    'pred_index': i,
-                }  
-                
-                # Store prediction with non overlapping predictions
-                preds_ok = preds_ok.append(entry, ignore_index=True)
-                
-                # Set overlap as solved
-                overlaps.loc[t, 'solved'] = True
-                    
-            # Case of multiple overlap
-            else:
-                # Extract all cases of overlaps
-                cases = overlaps[(overlaps['i'].isin([i,j])) | (overlaps['j'].isin([i,j]))]
-                # NB: some overlap cases might not be detected at first, e.g. in the case of pred i in top-left frame, overlap between top-right and bottom left is missed. 
-                # Include missed cases
-                # List indexes of included predictions
-                cases_indexes = np.unique(cases[['i', 'j']])
-                # Look for other rows with these predictions
-                all_cases = overlaps[(overlaps['i'].isin(cases_indexes)) | (overlaps['j'].isin(cases_indexes))]
-                
-                # Extract bboxes
-                bboxes = all_cases['bbox_union'].tolist()
-                
-                # TODO extract scores
-                
-                # Sequentially compute union of all bboxes
-                bbox_union = bboxes[0]
-                for r in range(len(bboxes)-1):
-                    bbox_union = compute_bbox_union(
-                        bbox_union, 
-                        bboxes[r+1],
-                    )  
-                
-                # Keep 1st prediction but with bbox_union and averaged score     
-                # Prepare new entry for df of non overlapping predictions
-                # Keep info of first frame for label, frame row and column
-                # Compute average of score
-                # Use newly computed bbox_corr
-                entry = {
-                    'frame_label': img_preds['frame_label'][i],
-                    'frame_row': img_preds['frame_row'][i],
-                    'frame_col': img_preds['frame_col'][i],
-                    'score': np.mean([(img_preds['score'][i], img_preds['score'][j])]),
-                    'bbox_corr': bbox_union,
-                    'pred_index': i,
-                }        
-                # Store prediction with non overlapping predictions
-                preds_ok = preds_ok.append(entry, ignore_index=True)
-                
-                # Set overlaps as solved
-                overlaps.loc[all_cases.index, 'solved'] = True
-    
-    return(preds_ok)
-
-
 def extract_rois(img, preds, dilate=2):
     """
     Extract image backgound of bbox predictions by semantic segmentation model. 
@@ -603,27 +308,42 @@ def extract_rois(img, preds, dilate=2):
     # Get image dimensions
     h, w = img.shape
     
+    # Create a blank mask for particles bbox and paste bbox
+    img_blank = np.zeros_like(img)
+
+    for i in preds.index:
+        # Extract bbox of each prediction
+        bbox = preds.loc[i, 'bbox_corr']
+        # Replace prediction slice by 1
+        img_blank[bbox[1]:bbox[3], bbox[0]:bbox[2]] = 1
+        
+    # Label bbox
+    img_lab = skimage.measure.label(img_blank, background=False, connectivity=2)
+
+    # Measure particles
+    bbox_meas = skimage.measure.regionprops(label_image=img_lab)
+    
     # Initiate new image with white background
     img_rois = np.ones_like(img)
     
-    # Loop over predictions and paste image bbox content
-    for i in preds.index:
+    # Loop over bbox and paste image bbox content
+    for i in range(len(bbox_meas)):
         
-        # Get bbox
-        bbox = preds.loc[i, 'bbox_corr']
+        # Get bbox of prediction
+        bbox = bbox_meas[i].bbox
         
         # Extract bbox content slice with `dilate` extra px on each side so particle can be dilated
         # Make sure that slice do not cross images borders
         slice_bbox = (
-            slice(max(0, bbox[1]-dilate), min(h, bbox[3]+dilate)), 
-            slice(max(0, bbox[0]-dilate), min(w, bbox[2]+dilate))
+            slice(max(0, bbox[0]-dilate), min(h, bbox[2]+dilate)), 
+            slice(max(0, bbox[1]-dilate), min(w, bbox[3]+dilate))
         )
         
         # Paste bbox content into new image
         img_rois[slice_bbox] = img[slice_bbox]
     
     return(img_rois)
-    
+
 
 def check_bbox_overlap(bb1, bb2):
     """
